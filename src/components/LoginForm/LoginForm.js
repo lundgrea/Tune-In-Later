bb1d9ea851cf5b95314ba67fe797694db7c54765
Aidan McKay <aidanmckay2000@gmail.com>
2019-09-04 15:36:37 -0600

    add initial form setup with onchange handler

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+export class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  loginInputs = (e) => {
+    const {name, value} = e.target
+    this.setState({ [name]: value })
+  }
+
+
+  render() {
+    return (
+      <section>
+        <form onSubmit={this.handleSubmit}>
+        <input 
+          type="text"
+          value={email}
+          name="email"
+          placeholder='example@example.com'
+          onChange={this.loginInputs}
+          />
+        <input 
+          type="password"
+          value={password}
+          name="password"
+          placeholder='Password'
+          onChange={this.loginInputs}
+          />
+        <button>Login</button>
+      </form>
+    </section>
+    )
+  }
+}
