3b8b2fafda687487a4b45e5de7997e3a5b03bf13
Chris Lane <pankokrumbs@gmail.com>
2019-09-05 17:30:36 -0600

    Add post fetch functionality to create a new user account with redux implementation
    Co-authored-by: Aidan McKay <aidanmckay2000@gmail.com>
    Co-authored-by: Alyssa Lundgren <lundgrea@gmail.com>

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,26 +1,47 @@
-export const getAlbums = async (artist) => {
+export const getAlbums = async artist => {
   try {
-    const response = await fetch(`https://itunes.apple.com/search?term=${artist}&entity=album`)
-    const data = await response.json()
-    return data
+    const response = await fetch(
+      `https://itunes.apple.com/search?term=${artist}&entity=album`
+    );
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.log(error.message);
     throw new Error("Failed to get albums");
   }
-}
-export const loginUser = async (user) => {
+};
+export const loginUser = async user => {
   try {
     const options = {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(user),
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       }
-    }
-      const response = await fetch('http://localhost:3001/api/v1/login', options)
-      const result = await response.json()
-      return result
-  } catch(error) {
-    throw new Error(error)
+    };
+    const response = await fetch("http://localhost:3001/api/v1/login", options);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw new Error("Incorrect login information");
   }
-}
+};
+
+export const postUser = async newUser => {
+  console.log("newUser :", newUser);
+  try {
+    const options = {
+      method: "POST",
+      body: JSON.stringify(newUser),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    const response = await fetch("http://localhost:3001/api/v1/users", options);
+    const result = await response.json();
+    console.log("result :", result);
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
