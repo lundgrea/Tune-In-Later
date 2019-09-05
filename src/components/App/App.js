225d2a3fb258c4c1ce4fc5396f846ddebd736d04
Chris Lane <pankokrumbs@gmail.com>
2019-09-05 16:19:59 -0600

    Install router dependencies, add browser router to index.js, and began work in App
    Co-authored-by: Aidan McKay <aidanmckay2000@gmail.com>
    Co-authored-by: Alyssa Lundgren <lundgrea@gmail.com>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import Search from '../../containers/Search/Search'
-import './App.css';
-import CardContainer from '../../containers/CardContainer/CardContainer';
-import LoginForm from '../LoginForm/LoginForm'
+import React, { Component } from "react";
+import Search from "../../containers/Search/Search";
+import "./App.css";
+import { Route, NavLink } from "react-router-dom";
+import CardContainer from "../../containers/CardContainer/CardContainer";
+import LoginForm from "../LoginForm/LoginForm";
 
 const App = () => {
   return (
     <>
-    <h1>HOLA</h1>
-    <Search />
-    <CardContainer />
-    <LoginForm />
+      <Route exact path="/" />
+      <h1>TUNE-IN LATER</h1>
+      <NavLink exact to="/login">
+        LOGIN
+      </NavLink>
+      <Search />
+      <CardContainer />
+      <Route exact path="/login" render={() => <LoginForm />} />
     </>
-  )
-}
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers'
+import { BrowserRouter} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(rootReducer, composeWithDevTools())
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
