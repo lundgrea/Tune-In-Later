d50d4602ca396fef77d4da6f55afdfff3ce44952
Alyssa Lundgren <lundgrea@gmail.com>
2019-09-09 13:59:00 -0600

    Finish action testing implementation

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -27,8 +27,8 @@ describe('actions', () => {
         }
       ]
     }
-    const result = actions.addAlbums(mockAlbums)
-    expect(result).toEqual(expectedAction)
+    const result = actions.addAlbums(mockAlbums);
+    expect(result).toEqual(expectedAction);
   });
 
 
@@ -44,7 +44,66 @@ describe('actions', () => {
         id: 4
       }
     }
-    const result = actions.login(mockUser)
+    const result = actions.login(mockUser);
+    expect(result).toEqual(expectedAction);
+  })
+
+  it('should have a type of LOGOUT', () => {
+    const expectedAction = {
+      type: 'LOGOUT'
+    }
+    const result = actions.logout();
+    expect(result).toEqual(expectedAction);
+  })
+
+  it('should have a type of TOGGLE_FAVORITE', () => {
+    const mockId = 7
+    const expectedAction = {
+      type: 'TOGGLE_FAVORITE',
+      id: 7
+    }
+    const result = actions.toggleFavorite(mockId);
+    expect(result).toEqual(expectedAction);
+  })
+
+  it('should have a CREATE_USER', () => {
+    const mockUser = {name: 'John', id: 18};
+    const expectedAction = {
+      type: 'CREATE_USER',
+      user: {name: 'John', id: 18}
+    }
+    const result = actions.createUser(mockUser)
     expect(result).toEqual(expectedAction)
+  })
+
+  it('should have a HANDLE_ERROR', () =>{
+    const mockError = 'WHOA, error';
+    const expectedAction = {
+      type: 'HANDLE_ERROR',
+      error: 'WHOA, error'
+    }
+    const results = actions.handleErrors(mockError);
+    expect(results).toEqual(expectedAction)
+
+  })
+
+  it('should have a STORE_FAVORITES', () => {
+    const mockAlbums = [{name: 'My Heart Will Go On', artist: 'Celine'}, {name: 'Blue', artist: 'BlueMan'}]
+    const expectedActions = {
+      type: 'STORE_FAVORITES',
+      albums: [{name: 'My Heart Will Go On', artist: 'Celine'}, {name: 'Blue', artist: 'BlueMan'}]
+    }
+    const results = actions.storeFavorites(mockAlbums);
+    expect(results).toEqual(expectedActions)
+  })
+
+  it('should have a DELETE_FAVORITE', () => {
+    const mockAlbumId = 8
+    const expectedActions = {
+      type: 'DELETE_FAVORITE',
+      albumId: 8
+    }
+    const results = actions.deleteFavorite(mockAlbumId)
+    expect(results).toEqual(expectedActions)
   })
 })
