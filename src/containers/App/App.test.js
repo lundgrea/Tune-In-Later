5b604434d031733bf869755859113fbdd9e727ac
Alyssa Lundgren <lundgrea@gmail.com>
2019-09-10 16:17:56 -0600

    Implement SignInForm testing

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -3,13 +3,12 @@ import { shallow, mount } from 'enzyme'
 import { App, mapStateToProps, mapDispatchToProps } from './App';
 import { MemoryRouter } from 'react-router';
 import { logout, storeFavorites } from '../../actions';
+import { getFavorites } from "../../apiCalls/apiCalls";
 
 
 describe('App', () => {
-
   let wrapper;
   const mockAlbums = [{trackName: 'Hello', genre: 'Rock'}, {trackName: 'Yellow', genre: 'Folk'}];
-
   const mockLogOut = jest.fn();
   const mockStoreFavorites = jest.fn();
 
@@ -59,38 +58,39 @@ describe('mapStateToProps', ()  => {
     const mappedProps = mapStateToProps(mockState);
     expect(mappedProps).toEqual(expected)
   })
-    it('should have a user saved if a user is logged in', () => {
-      const mockState = {
-        albums: [{albumName: 'Johanna', genre: 'Rock'}, {albumName: 'Eoah', genre: 'Country'}],
-        error: 'error',
-        user: 1
-      }
-      const expected = {
-        albums: [{albumName: 'Johanna', genre: 'Rock'}, {albumName: 'Eoah', genre: 'Country'}],
-        error: 'error',
-        user: 1
-      }
-      const mappedProps = mapStateToProps(mockState);
-      expect(mappedProps).toEqual(expected)
+  
+  it('should have a user saved if a user is logged in', () => {
+    const mockState = {
+      albums: [{albumName: 'Johanna', genre: 'Rock'}, {albumName: 'Eoah', genre: 'Country'}],
+      error: 'error',
+      user: 1
+    }
+    const expected = {
+      albums: [{albumName: 'Johanna', genre: 'Rock'}, {albumName: 'Eoah', genre: 'Country'}],
+      error: 'error',
+      user: 1
+    }
+    const mappedProps = mapStateToProps(mockState);
+    expect(mappedProps).toEqual(expected)
   })
 })
 
 describe('mapDispatchToProps', () => {
-    it('calls logout with an logout action when logout is called', () => {
-      const mockDispatch = jest.fn();
-      const actionToDispatch = logout();
-      const mappedProps = mapDispatchToProps(mockDispatch);
-      mappedProps.logout();
-      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
-    });
+  it('calls logout with an logout action when logout is called', () => {
+    const mockDispatch = jest.fn();
+    const actionToDispatch = logout();
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.logout();
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  });
 
-    it('calls storeFavorites with an array of favorites when storeFavorites is called', () => {
-      const mockDispatch = jest.fn();
-      const actionToDispatch = storeFavorites([{albumName: 'Johanna', genre: 'Rock'}, {albumName: 'Eoah', genre: 'Country'}]);
-      const mappedProps = mapDispatchToProps(mockDispatch);
-      mappedProps.storeFavorites([{albumName: 'Johanna', genre: 'Rock'}, {albumName: 'Eoah', genre: 'Country'}]);
-      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
-    });
+  it('calls storeFavorites with an array of favorites when storeFavorites is called', () => {
+    const mockDispatch = jest.fn();
+    const actionToDispatch = storeFavorites([{albumName: 'Johanna', genre: 'Rock'}, {albumName: 'Eoah', genre: 'Country'}]);
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.storeFavorites([{albumName: 'Johanna', genre: 'Rock'}, {albumName: 'Eoah', genre: 'Country'}]);
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  });
 })
 
 
