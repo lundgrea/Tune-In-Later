b1a13cc20d2240de0e7f1f705be5cf9559f4948a
Aidan McKay <aidanmckay2000@gmail.com>
2019-09-10 15:11:59 -0600

    add test for snapshot and mapStatToProps for Card

--- src/containers/Card/Card.test.js
+++ src/containers/Card/Card.test.js
@@ -1,17 +1,55 @@
 import React from 'react';
-// import  { shallow } from 'enzyme';
-import Card from './Card';
+import  { shallow } from 'enzyme';
+import { Card, mapDispatchToProps, mapStateToProps } from './Card';
 
 describe('Card', () => {
-  it('should', () => {
-   
+  let wrapper;
 
-//mapStateToProps
+  const mockFavorites = [
+    {album_name: 'mirrorland', album_id: 12345678, primary_genre_name: 'rap'}
+  ];
 
-//mapDispatchToProps
+  const mockUser = {
+    id: 1, name: 'aidan', email: 'aidanmckay2000@gmail.com'
+  };
 
-//snapshot w/conditional rendering
-
+  const mockHandleErrors = jest.fn()
+  const mockStoreFavorites = jest.fn()
+  const mockToggleFavorite = jest.fn()
+  beforeEach(() => {
+    wrapper = shallow(
+      <Card 
+        favorites={mockFavorites}
+        handleErrors={mockHandleErrors}
+        storeFavorites={mockStoreFavorites}
+        album_id={1}
+        album_name='MirrorLand'
+        artist_name='EarthGang'
+        artwork_url='asdf'
+        content_advisory_rating='explicit'
+        isFavorite={true}
+        primary_genre_name='Rap'
+        release_date='09/05/2019'
+        toggleFavorite={mockToggleFavorite}
+        user={mockUser}
+      />
+    )
   })
-})
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot() 
+  });
+
+  describe('mapStateToProps', () => {
+    it('should return an array of favorite album objects', () => {
+      const mockState = {
+        favorites: mockFavorites
+      }
+      const expected = {
+        favorites: mockFavorites
+      }
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps).toEqual(expected)
+    })
+  });
+});
 
